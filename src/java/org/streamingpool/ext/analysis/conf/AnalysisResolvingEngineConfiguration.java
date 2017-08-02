47ab98627e0d59e8c3ccaa82a796cae451e7b7be
acalia <andrea.calia@cern.ch>
2017-08-02 16:54:47 +0200

    moved resolvers to tensorics-core. Now the analysis specifi resolvers are provided as Beans

--- src/java/org/streamingpool/ext/analysis/conf/AnalysisResolvingEngineConfiguration.java
+++ src/java/org/streamingpool/ext/analysis/conf/AnalysisResolvingEngineConfiguration.java
@@ -8,21 +8,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.streamingpool.ext.analysis.resolver.AssertionGroupResolver;
 import org.streamingpool.ext.analysis.resolver.AssertionResolver;
-import org.tensorics.core.resolve.engine.ResolvingEngine;
-import org.tensorics.core.resolve.engine.ResolvingEngines;
-import org.tensorics.core.resolve.resolvers.IterableResolvingExpressionResolver;
-import org.tensorics.core.resolve.resolvers.PickResolver;
-import org.tensorics.core.resolve.resolvers.PredicateConditionResolver;
-import org.tensorics.core.resolve.resolvers.WindowedExpressionResolver;
 
 @Configuration
 public class AnalysisResolvingEngineConfiguration {
 
     @Bean
-    public ResolvingEngine createResolvingEngine() {
-        return ResolvingEngines.defaultEngineWithAdditional(new AssertionGroupResolver(), new AssertionResolver(),
-                new PredicateConditionResolver<>(), new IterableResolvingExpressionResolver<>(),
-                new WindowedExpressionResolver(), new PickResolver<>());
+    public AssertionGroupResolver assertionGroupResolver() {
+        return new AssertionGroupResolver();
+    }
+
+    @Bean
+    public AssertionResolver assertionResolver() {
+        return new AssertionResolver();
     }
 
 }
